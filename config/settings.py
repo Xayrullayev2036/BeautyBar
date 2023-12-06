d92eb28df739af6e98fad0ffc59c6e0d3cfdfbf3
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2023-12-06 20:17:21 +0500

    codes has been updated

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "beauty_barr",
+        'NAME': "beauty_bar",
         'USER': "postgres",
         'PASSWORD': '1',
         'HOST': "localhost",
