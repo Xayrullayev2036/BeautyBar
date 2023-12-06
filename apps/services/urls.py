d1e951df9fa7a0ba6d69670620b4899e6dfda014
Xayrullayev2036 <mr.xayrullayev2036@gmail.com>
2023-12-06 11:56:39 +0500

    commit changes

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-
 from apps.services.views import ServiceList1APIView, ServiceCreateAPIView, ServiceUpdateAPIView, ServiceDeleteAPIView, \
     ServiceImageView, ServiceListAPIView
 
@@ -10,8 +9,8 @@ urlpatterns = [
     path('service/<int:pk>/image', ServiceImageView.as_view(), name='service-image-upload'),
     path('service/get/', ServiceListAPIView.as_view()),
 
-    # path('service/api/update/<int:pk>/', ServiceUpdateAPIView.as_view()),
-    # path('service/api/delete/<int:pk>', ServiceDeleteAPIView.as_view())
+# path('service/api/update/<int:pk>/', ServiceUpdateAPIView.as_view()),
+# path('service/api/delete/<int:pk>', ServiceDeleteAPIView.as_view())
 
 from apps.services.views import ServiceGetAPIView, ServiceCreateAPIView, ServiceUpdateAPIView, ServiceDeleteAPIView, \
     CategoryGetAPIView, ServiceOwnerGetAPIView
@@ -25,7 +24,6 @@ urlpatterns = [
     path('service/api/delete/<int:pk>', ServiceDeleteAPIView.as_view())
 
 ]
-
 
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAxNTEzMjE0LCJpYXQiOjE3MDE1MDYwMTQsImp0aSI6ImU0ZmIwNzYzMTRiZjQyYjlhYzg2NmFhNTIwNjQwZjQ1IiwidXNlcl9pZCI6MTN9.N_-FAddygaz7eRyi7BHqiqKD6nwn0njDHxdqMJ2h014
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAxNTEzNTA3LCJpYXQiOjE3MDE1MDYzMDcsImp0aSI6ImZhMzIyN2I4NDVmNTQ3MGI5YTMwM2ZjZTQ4YjhjZjI4IiwidXNlcl9pZCI6MTR9.RBXjHI0jNSB7ZAZ4YDbo_GKQw4zyIGCNz0iGzwUxQnM
